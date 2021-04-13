982ea1c7afac2f94b9dc4e13113fb6f773362170
kevskillz <66384762+kevskillz@users.noreply.github.com>
2021-04-12 20:07:18 -0400

    Update types_.h
    commented out 4 constants... defined in balls.cpp

--- types_.h
+++ types_.h
@@ -4,6 +4,8 @@
 
 #ifndef POOL_TYPES__H
 #define POOL_TYPES__H
+
+/*
 // RADIUS CONSTANT
 int Radius = 1;
 
@@ -13,6 +15,7 @@ int ThresDist = 2;
 // x and y max constant in pixels
 int XMAX = 1000;
 int YMAX = 1000;
+*/
 
 // defines all the typedefs
 typedef float Pixel;
