94f855247c00c9eeceabe25da7c53b914027268d
kevskillz <66384762+kevskillz@users.noreply.github.com>
2021-04-17 22:17:40 -0400

    Update main.cpp
    added collision with multiple balls on board, cleaned up code

--- main.cpp
+++ main.cpp
@@ -12,14 +12,22 @@
 #include <SDL2/SDL.h>
 
 
+// initializes innovation
+int ball::nextInnovation = 0;
+
+// initializes vector of all balls
+std::vector<std::reference_wrapper<ball>> ball::balls = {};
+
 int main(int argc, char* argv[]) {
 
-	// y starting pos needs to be > 60 or else bugs occur
-	ball Ballmain = ball(500.0f, 400.0f, TWO, 270, SOLID, 0, 0, 0);
-	render Rendermain = render();
+	// creates renderer, balls
 
-	ball Ball2 = ball(730.0f, 500.0f, FIVE, 270, SOLID, 1, 0, 0);
-	std::vector<std::reference_wrapper<ball>> balls = { Ballmain, Ball2 };
+	render Rendermain = render();
+	ball Ballmain = ball(500.0f, 60.0f, TWO, SOLID, 0, 0);
+	ball Ball2 = ball(730.0f, 300.0f, FIVE, SOLID, 0, 0);
+	ball Ball3 = ball(850.0f, 200.0f, FIVE, BLACK, 0, 0);
+
+
 
 
 
@@ -29,6 +37,8 @@ int main(int argc, char* argv[]) {
 
 	SDL_Window* win = NULL;
 	SDL_Renderer* renderer = NULL;
+
+
 
 	int posX = 100, posY = 100, width = 900, height = 900;
 
@@ -40,10 +50,10 @@ int main(int argc, char* argv[]) {
 
 
 	Uint32 framestart;
-	
+
 	while (true) {
 		framestart = SDL_GetTicks();
-		
+
 
 		SDL_Event e;
 		if (SDL_PollEvent(&e)) {
@@ -56,103 +66,41 @@ int main(int argc, char* argv[]) {
 			}
 		}
 
-		// MOVE
-		Ballmain.move(balls);
-		Ball2.move(balls);
+		// moves every ball
+
+		for (ball& b : ball::balls) b.move();
 
 
-		// ADD COLLISION WITH MULTIPLE BALLS LATER
-		if (Ballmain.checkForCollisionBall(Ball2)) {
-			Ballmain.collisionBall(Ball2);
-
-		}
 		
+
+
 
 		// sets bg to pink
 		SDL_SetRenderDrawColor(renderer, 255, 23, 255, 0);
 
+
+		// clears screen
 		SDL_RenderClear(renderer);
 
-		
-
-		// goes through each ball to see if one their velocities is over 0
-
-		Rendermain.drawCircle(renderer, Ballmain, 30, true);
-		Rendermain.drawCircle(renderer, Ball2, 30, true);
-
-		
-		
-		
-		// draws circles
 
 
+		// goes through each ball and draws it
+
+		for (ball& b : ball::balls) {
+			Rendermain.drawCircle(renderer, b, 30, true);
+		}
+
+
+
+		// presents screen
 		SDL_RenderPresent(renderer);
 
 
+		// sets framerate to 144 by delaying each frame if needed
 		Uint32 frametime = SDL_GetTicks() - framestart;
 		if (1000 / 144 > frametime) SDL_Delay(1000 / 144 - frametime);
 
-		/*
-
-		// moves the balls
-
-		// removes one from the velocity
-		Ballmain.velocity -= 1;
-
-		//todo to make this more effeicient for training in move just multiply the movement by new x and y by the velocity
-		for (int i = 0; i < Ballmain.velocity; i++) {
-			// sets bg to pink
-			SDL_SetRenderDrawColor(renderer, 255, 23, 255, 0);
-
-			SDL_RenderClear(renderer);
-
-			// moves the main ball
-			Ballmain.move(balls);
-
-			// goes through each ball to see if one their velocities is over 0
-			for (ball& Ball : balls) {
-				if (Ball.velocity > 0) {
-					// moves the ball
-					Ball.move(balls);
-
-					// decreases velocity
-					Ball.velocity -= 1;
-				}
-				Rendermain.drawCircle(renderer, Ball, 30, true);
-			}
-			// draws circles
-			Rendermain.drawCircle(renderer, Ballmain, 30, true);
-
-
-			SDL_RenderPresent(renderer);
-		}
-		SDL_SetRenderDrawColor(renderer, 255, 23, 255, 0);
-
-		SDL_RenderClear(renderer);
-
-		// does other balls if first ball stops
-		for (ball& Ball : balls) {
-			if (Ball.velocity > 0) {
-				// sets bg to pink
-
-
-				// moves the ball
-				Ball.move(balls);
-
-				// decreases velocity
-				Ball.velocity -= 1;
-
-			}
-			Rendermain.drawCircle(renderer, Ball, 30, true);
-		}
-		Rendermain.drawCircle(renderer, Ballmain, 30, true);
-
-		SDL_RenderPresent(renderer);
-
-
-		//todo this is not computing efficent at all, so change all of this latero n
-
-		*/
+	
 	}
 
 
