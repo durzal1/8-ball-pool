dfca719292c108fd1d64d96863215f53e910c804
kevskillz <66384762+kevskillz@users.noreply.github.com>
2021-04-27 13:17:05 -0400

    fixed goal detection

--- goal.cpp
+++ goal.cpp
@@ -2,10 +2,12 @@
 // Created by zacky on 4/19/2021.
 //
 #include "goal.h"
+#include <iostream>
+
 
 
 // constructor
-goal::goal(Pixel x, Pixel y, int Radius){
+goal::goal(Pixel x, Pixel y, int Radius) {
     this->x = x;
     this->y = y;
     this->Radius = Radius;
@@ -13,6 +15,16 @@ goal::goal(Pixel x, Pixel y, int Radius){
 
 // returns point
 SDL_Point goal::getPixels() {
-    return SDL_Point({int(x), int(y)});
+    return SDL_Point({ int(x), int(y) });
 }
-//
+
+bool goal::isGoal(Pixel bx, Pixel by, int r) {
+
+    // |sqrt((px - bx)^2 + (py - by)^2 - r| gives distance between point (center of goal)
+    // and circle we need to see if it is approaching zero. Since we have two sides of the
+    // equation to check if it is < 2 (threshold value), we do not the sqrt operation (costly
+    // operation)!
+     
+
+    return fabs(powf(x - bx, 2) + powf(y - by, 2) - r) < 2;
+}
